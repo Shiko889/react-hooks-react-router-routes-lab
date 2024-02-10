faf02aed4ee6578cf24ca9c95bed34a47f44e812
shiko <sarah.tharao@student.moringaschool.com>
2024-02-10 19:24:32 +0300

    Completed assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+const directorList = directors.map((director, index) => (
+<div key={index}>
+<h1>{director.name}</h1>
+<ul>
+{director.movies.map((movie, movieIndex) => (
+<li key={movieIndex}>{movie}</li>
+))}
+</ul>
+</div>
+));
+
+
+return (
+<div>
+<h1>Directors Page</h1>
+
+{directorList}
+</div>
+);
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,24 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const movieList = movies.map((mov, index) => (
+    <div key={index}>
+      <h1>{mov.title}</h1>
+      <h2>{mov.time}</h2>
+      <ul>
+        {mov.genres.map((genre, genreIndex) => (
+          <li key={genreIndex}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movieList}
+    </div>
+  );
 }
 
 export default Movies;
